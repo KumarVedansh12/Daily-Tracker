4f703d17bccd1f928dcc83dffab948d58db018ea
KumarVedansh12 <vedanshupadhyay1218@gmail.com>
2026-08-01 23:39:06 +0530

    Time: 0 ms (100%), Space: 133.4 MB (59.2%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int>less;
-        vector<int>gre;
-        int e=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]<pivot){
                 less.push_back(nums[i]);
             }
-            else if(nums[i]>pivot){
-                gre.push_back(nums[i]);
-            }
-            else{
-                e++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==pivot){
+                less.push_back(pivot);
             }
         }
-        while(e--){
-            less.push_back(pivot);
-        }
-        for(int i=0;i<gre.size();i++){
-            less.push_back(gre[i]);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>pivot){
+                less.push_back(nums[i]);
+            }
         }
         return less;
     }
